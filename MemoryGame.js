6271bc9e90949f9c2942d1e4c3b53afb5b4a9097
Rebekah Wise <RebekahWise@MacBook-Pro.local>
2024-08-07 22:56:55 -0400

    matching game

--- MemoryGame.js
+++ MemoryGame.js
@@ -50,27 +50,58 @@ function createDivsForColors(colorArray) {
     newDiv.classList.add(color);
 
     // call a function handleCardClick when a div is clicked on
-    newDiv.addEventListener("click", handleCardClick);
+  newDiv.addEventListener("click", handleCardClick);
 
     // append the div to the element with an id of game
     gameContainer.append(newDiv);
   }
 }
+// function onlyTwoCards (event){if (count < 2){cardColor(event)}
+// else {alert("You can only click two cards at a time!")
+// return;}}
+// function onlyTwoCards() {alert("You can only click two cards at a time!");
+//     return;}
+    
 
-// TODO: Implement this function!
-let count = 0;
-function handleCardClick(event) {
-   // let cardColor = event.target.className;
-   if (!event.target.classList.contains("match")){
+//if the div does not have a class of match, the background color will change to match the 
+//class color and a class of "clicked" will be added. the count will increase by one    
+function cardColor(event){if (!event.target.classList.contains("match")){
     event.target.style.backgroundColor = event.target.className;
-    count++;
-    for (let i = 0; i<=2; i++)
-    if (count = 2){count = 0;
-    //     const resets = document.querySelectorAll("div");
-    //     for(let reset of resets){
-    //         if(!reset.classList.contains("match")){reset.style.backgroundColor = null};
-    // }}
-}}}
+  event.target.classList.add("clicked")}
+count++;}  
+
+//resets the background to no color and removes the "clicked" class
+function resetCards(){
+const cards = document.querySelectorAll(".clicked");  
+cards.forEach((card) => {card.style.backgroundColor = '';
+card.classList.remove("clicked");
+})
+}    
+//resets cards after 2 seconds
+function isNotMatch(){setTimeout(resetCards,2000);
+ const allDiv = document.querySelectorAll("div"); 
+ allDiv.forEach((divs) =>{divs.removeEventListener("click", function(){alert("NOT A MATCH!")}); 
+})}   
+
+let count = 0;
+
+function handleCardClick(event) {
+ cardColor(event);
+//when two cards are clicked their class names are compared for a match. If they are a 
+//match they are given the class of match and left 'face up'. the class of "clicked" is removed
+if(count === 2){
+  const cards = document.querySelectorAll(".clicked");
+  if(cards[0].className === cards[1].className){cards.forEach((card) => {card.classList.add("match");
+  card.classList.remove("clicked")});
+}
+//if it's not a match, the not match function runs and the count is reset to 0
+else {isNotMatch();}
+count = 0;}
+
+}
+  
+ 
+
 
 // when the DOM loads
 createDivsForColors(shuffledColors);
