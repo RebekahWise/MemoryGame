8ad3f25defaf944aae06e424042a60ddae76348a
Rebekah Wise <RebekahWise@MacBook-Pro.lan>
2024-08-25 16:02:04 -0400

    add restart and turn counter

--- MemoryGame.js
+++ MemoryGame.js
@@ -10,124 +10,124 @@ const COLORS = [
   "blue",
   "green",
   "orange",
-  "purple"
+  "purple",
 ];
 
-// here is a helper function to shuffle an array
-// it returns the same array with values shuffled
-// it is based on an algorithm called Fisher Yates if you want ot research more
+// Shuffle function
 function shuffle(array) {
   let counter = array.length;
-
-  // While there are elements in the array
   while (counter > 0) {
-    // Pick a random index
     let index = Math.floor(Math.random() * counter);
-
-    // Decrease counter by 1
     counter--;
-
-    // And swap the last element with it
     let temp = array[counter];
     array[counter] = array[index];
     array[index] = temp;
   }
-
   return array;
 }
 
 let shuffledColors = shuffle(COLORS);
 
-// this function loops over the array of colors
-// it creates a new div and gives it a class with the value of the color
-// it also adds an event listener for a click for each card
+// Create divs for colors
 function createDivsForColors(colorArray) {
   for (let color of colorArray) {
-    // create a new div
     const newDiv = document.createElement("div");
-
-    // give it a class attribute for the value we are looping over
     newDiv.classList.add(color);
-
-    // call a function handleCardClick when a div is clicked on
-  newDiv.addEventListener("click", handleCardClick);
-
-    // append the div to the element with an id of game
+    newDiv.addEventListener("click", handleCardClick);
     gameContainer.append(newDiv);
   }
 }
-// function onlyTwoCards (event){if (count < 2){cardColor(event)}
-// else {alert("You can only click two cards at a time!")
-// return;}}
-// function onlyTwoCards() {alert("You can only click two cards at a time!");
-//     return;}
-    
-
-//if the div does not have a class of match, the background color will change to match the 
-//class color and a class of "clicked" will be added. the count will increase by one    
-function cardColor(event){if (!event.target.classList.contains("match")){
-    event.target.style.backgroundColor = event.target.className;
-  event.target.classList.add("clicked")}
-count++;}  
-
-//resets the background to no color and removes the "clicked" class
-function resetCards(){
-const cards = document.querySelectorAll(".clicked");  
-cards.forEach((card) => {card.style.backgroundColor = '';
-card.classList.remove("clicked");
-})
-}    
-//resets cards after 2 seconds
-function isNotMatch(){setTimeout(resetCards,2000);
- const allDiv = document.querySelectorAll("div"); 
- allDiv.forEach((divs) =>{divs.removeEventListener("click", function(){alert("NOT A MATCH!")}); 
-})}   
 
 let count = 0;
-let turns = 0;
+let turnNumber = 0;
+let firstCard = null;
+let secondCard = null;
+
+function winner() {
+  let matches = document.querySelectorAll(".match").length;
+  let flipped = document.querySelectorAll("div").length - 1;
+  if (matches === flipped) {
+    setTimeout(function () {
+      highScore();
+    }, 2000);
+    setTimeout(function () {
+      alert("YOU WIN!!!");
+    }, 1000);
+  }
+}
 
 function handleCardClick(event) {
- cardColor(event);
-//when two cards are clicked their class names are compared for a match. If they are a 
-//match they are given the class of match and left 'face up'. the class of "clicked" is removed
-if(count === 2){
-  const cards = document.querySelectorAll(".clicked");
-  if(cards[0].className === cards[1].className){cards.forEach((card) => {card.classList.add("match");
-  card.classList.remove("clicked")});
-  let matches = ((document.querySelectorAll(".match")).length);
-let flipped = ((document.querySelectorAll("div")).length-1);
-if(matches === flipped) {setTimeout(alert("YOU WIN!!!"),2000)};
-}
-//if it's not a match, the not match function runs and the count is reset to 0
-else {isNotMatch();}
-count = 0;
-turns++;
-let turn = document.querySelector("h2");
-turn.innerText = "Turns: "+ `${turns}`;
+  if (
+    count < 2 &&
+    !event.target.classList.contains("clicked") &&
+    !event.target.classList.contains("match")
+  ) {
+    event.target.style.backgroundColor = event.target.className;
+    event.target.classList.add("clicked");
+    count++;
+
+    if (count === 1) {
+      firstCard = event.target;
+    } else if (count === 2) {
+      secondCard = event.target;
+      if (firstCard.className === secondCard.className) {
+        firstCard.classList.add("match");
+        secondCard.classList.add("match");
+        resetCards();
+        winner();
+      } else {
+        setTimeout(isNotMatch, 1000);
+      }
+    }
+  }
 }
 
-// let cards = ((document.querySelectorAll(".match")).length);
-// let flipped = ((document.querySelectorAll("div")).length-1);
-// if(cards = flipped) {alert("YOU WIN!!!")};
+function resetCards() {
+  count = 0;
+  firstCard = null;
+  secondCard = null;
+  const clickedCards = document.querySelectorAll(".clicked");
+  clickedCards.forEach((card) => {
+    card.classList.remove("clicked");
+    turnNumber++;
+    let turn = document.querySelector("#turns");
+    let turns = turnNumber / 2;
+    turn.innerText = "Turns: " + `${turns}`;
+  });
 }
-  
 
-// when the DOM loads
+function isNotMatch() {
+  firstCard.style.backgroundColor = "azure";
+  secondCard.style.backgroundColor = "azure";
+  resetCards();
+}
+// const reset = createDivsForColors(shuffledColors);
+//Only addd one button because I don't see the purpose of 2 buttons that do the same thing
+const button = document.getElementById("restart");
+button.addEventListener("click", reload);
+
+function reload() {
+  window.location.reload();
+}
+
+// loadList();
+// When the DOM loads
 createDivsForColors(shuffledColors);
 
-// - Clicking a card should change the background color 
-//to be the color of the class it has.
-
-// - Users should only be able to change at most two cards 
-//at a time.
-
-
-
-// - Clicking on two matching cards should be a “match” — 
-//those cards should stay face up.
-// - When clicking two cards that are not a match, they should 
-//stay turned over for at least 1 second before they hide the 
-//color again. You should make sure to use a  so that you can 
-//execute code after one second.
-    
-//     ***setTimeout***
+let bestScore = JSON.parse(localStorage.getItem("highScore"));
+if (bestScore === null) {
+  bestScore === 0;
+}
+let scores = document.querySelector("#score");
+scores.innerText = "Best Score: " + `${bestScore}`;
+function highScore() {
+  if (turns < bestScore || bestScore === 0 || bestScore === null) {
+    bestScore = turnNumber / 2;
+    localStorage.setItem("highScore", JSON.stringify(bestScore));
+    alert("Awesome! You beat the high score!");
+    let scores = document.querySelector("#score");
+    scores.innerText = "Best Score: " + `${bestScore}`;
+  } else {
+    alert("You didn't beat the high score.");
+  }
+}
